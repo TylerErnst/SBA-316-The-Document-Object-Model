22186610d12fa7929ff588a12df13d5fa6e51acd
TylerErnst <tcernst5@gmail.com>
2024-04-04 16:29:54 -0600

    Moved some code around

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let canvasState = true;
 let color = '#FF0000';
-
-
 
 // Cache at least one element using selectElementById.
 const canvas = document.getElementById('canvas');
@@ -45,6 +16,28 @@ const clearButton = colorSelect.lastElementChild;
 const darkButton = document.getElementById('darkButton')
 const resolutionButton = document.getElementById('resolutionButton');
 const maxResolutionButton = resolution.lastElementChild;
+
+
+
+//Initial setup
+
+//Default resolution values
+let resoultionWidth = 400;
+let resoultionHeight = 400;
+//default pixel size
+let pixelSize = 10;
+//calculate pixels needed to fill canvas using resolution height and width
+let pixels = (resoultionHeight/pixelSize) * (resoultionWidth/pixelSize);
+//Create Canvas
+setResolution(resoultionWidth, resoultionHeight);
+
+
+
+
+
+
+
+
 // Iterate over a collection of elements to accomplish some task.
 //          Radio buttons?
 //          Bubble?
@@ -91,7 +84,9 @@ function  clearCanvas(canvas, pixelCount){
 }
 
 
-// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
+
+
 // Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
 // Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
 darkButton.addEventListener("click", (e) => {
@@ -121,12 +116,18 @@ clearButton.addEventListener("click", (event) => {
     setResolution (width, height);
 });
 // Register at least two different event listeners and create the associated event handler functions.
-//          See above 2
+//          See above 2+
 
 // Use at least two Browser Object Model (BOM) properties or methods. done
 //      Max resoultion of canvas
 //      screen.availWidth
 //      screen.availHeight
+maxResolutionButton.addEventListener("click", (event) => {
+    let width = screen.availWidth * .9;
+    let height = screen.availHeight * .7;
+    setResolution(width, height);    
+});
+
 // Include at least one form and/or input with HTML attribute validation. done
 // Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
 // Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
@@ -173,25 +174,10 @@ resolutionButton.addEventListener("click", (event) => {
     setResolution(resoultionWidth, resoultionHeight);
 });
 
-maxResolutionButton.addEventListener("click", (event) => {
-    let width = screen.availWidth * .9;
-    let height = screen.availHeight * .7;
-    setResolution(width, height);    
-});
 
 
 
-//Initial setup
 
-//Default resolution values
-let resoultionWidth = 400;
-let resoultionHeight = 400;
-//default pixel size
-let pixelSize = 10;
-//calculate pixels needed to fill canvas using resolution height and width
-let pixels = (resoultionHeight/pixelSize) * (resoultionWidth/pixelSize);
-//Create Canvas
-setResolution(resoultionWidth, resoultionHeight);
 
 
 
